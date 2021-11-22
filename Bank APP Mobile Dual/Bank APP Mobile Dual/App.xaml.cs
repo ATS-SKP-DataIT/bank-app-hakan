fbe8800c7e41e4229b3ca0752b993ab8d8c9e908
Hakan Sahin <Hakan.sahin994@gmail.com>
2021-11-22 11:01:44 +0100

    First

---
+++ Bank APP Mobile Dual/Bank APP Mobile Dual/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Bank_APP_Mobile_Dual.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bank_APP_Mobile_Dual
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //   MainPage = new MainPage();
+            MainPage = new FrontPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Bank APP Mobile Dual/Bank APP Mobile Dual/Pages/FrontPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bank_APP_Mobile_Dual.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FrontPage : ContentPage
+    {
+        public FrontPage()
+        {
+            InitializeComponent();
+
+            Title = "Bank Application";
+            BackgroundColor = Color.DarkSlateBlue;
+            int currentBalance = 15000;
+            int accountNumber = 1293923942;
+            string accountName = "Main Account";
+
+            Frame depositLayout = new Frame {
+                Padding = 1,
+                CornerRadius = 3,
+                Content = new StackLayout
+                {
+                    Spacing = 3,
+                    BackgroundColor = Color.DarkSlateBlue,
+                    Children =
+                    {
+                        LabelMaker($"Deposit Amount:", 18, TextAlignment.Start, TextAlignment.Start, Color.White, FontAttributes.None),
+                        new Entry{}
+                    }
+                }
+            };
+
+            var underLayout = new StackLayout
+            {
+                Margin = new Thickness(10),
+                Spacing = 25,
+                BackgroundColor = Color.DarkSlateBlue,
+                Children =
+                {
+                    LabelMaker($"Account Name: {accountName}", 14, TextAlignment.Center, TextAlignment.Start, Color.White, FontAttributes.None),
+                    new Frame{
+                        Padding = 1,
+                        CornerRadius = 3,
+                        Content = new StackLayout
+                        {
+                            Spacing = 3,
+                            BackgroundColor = Color.DarkSlateBlue,
+                            Children =
+                            {
+
+                                LabelMaker($"Account Number: {accountNumber}", 14, TextAlignment.Center, TextAlignment.Start, Color.White, FontAttributes.None),
+                                LabelMaker($"Current Balance: {currentBalance}", 18, TextAlignment.Center, TextAlignment.Start, Color.White, FontAttributes.Bold),
+                                AccountDropDown(accountNumber),
+                            }
+                        },
+                    },
+                    depositLayout,
+                },
+            };
+
+           
+
+            var layout = new StackLayout
+            {
+                Margin = new Thickness(20),
+                Spacing = 25,
+                BackgroundColor = Color.DarkSlateBlue,
+                Children =
+                {
+                    LabelMaker("Deposit", 22, TextAlignment.Center, TextAlignment.Start, Color.White, FontAttributes.Bold),
+                    underLayout,
+
+                }
+            };
+            Content = layout;
+        }
+
+        public Label LabelMaker(string txt, int size, TextAlignment horizontal, TextAlignment vertical, Color txtColor, FontAttributes fontAttributes)
+        {
+            return new Label
+            {
+                Text = txt,
+                FontSize = size,
+                HorizontalTextAlignment = horizontal,
+                VerticalTextAlignment = vertical,
+                TextColor = txtColor,
+                FontAttributes = fontAttributes,
+            };
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private Picker AccountDropDown(int accountNumber)
+        {
+            List<string> AccountInfo = new List<string>();
+            AccountInfo.Add(accountNumber.ToString());
+            var _picker = new Picker();
+            foreach (var item in AccountInfo)
+            {
+                _picker.Items.Add(item);
+            }
+            _picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+            return _picker;
+        }
+    } 
+}
