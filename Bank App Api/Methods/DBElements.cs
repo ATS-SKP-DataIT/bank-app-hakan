666e9c375f7ccf557616bd4c49e5e959b51a4a99
Hakan Sahin <Hakan.sahin994@gmail.com>
2021-12-01 13:19:00 +0100

    Initial

---
+++ Bank App Api/Methods/DBElements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank_App_Api.Methods
+{
+    public class DBElements : IDBElements
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+        public string Collection { get; set; }
+    }
+    public interface IDBElements
+    {
+        string ConnectionString { get; set; }
+        string Database { get; set; }
+        string Collection { get; set; }
+    }
+}
---
+++ Bank App Api/Methods/UserElements.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank_App_Api.Methods
+{
+    public class UserElements
+    {
+    }
+    public class LoginModel : ILoginModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        public List<string> Recovery { get; set; }
+
+    }
+    public interface ILoginModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        ObjectId Id { get; set; }
+        string UserName { get; set; }
+        string Email { get; set; }
+
+        string Password { get; set; }
+        List<string> Recovery { get; set; }
+    }
+
+
+}
+
