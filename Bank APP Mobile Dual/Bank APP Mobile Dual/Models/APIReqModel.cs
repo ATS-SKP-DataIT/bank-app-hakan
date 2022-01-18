5db702f12f8660dfb784b240c87125f58c340de5
Hakan Sahin <Hakan.sahin994@gmail.com>
2022-01-18 11:48:30 +0100

    Initial Encryption added

---
+++ Bank APP Mobile Dual/Bank APP Mobile Dual/Models/APIReqModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_APP_Mobile_Dual.Models
+{
+    public class APIReqModel : IAPIReqModel
+    {
+        public string Json { get; set; }
+    }
+    public interface IAPIReqModel
+    {
+        string Json { get; set; }
+    }
+}
